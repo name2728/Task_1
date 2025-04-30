39dab55f2fa3b31d9c88767bc87996e861f42e50
Komarovskaya Yuliya <yuliya.komarovskaya.by@gmail.com>
2025-04-30 12:23:42 +0500

    All methods

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -4,14 +4,13 @@ package org.example;
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
 
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
-        }
+Calculator calculator = new Calculator();
+System.out.println("5+5 = "+calculator.add(5,5));
+System.out.println("5-1 =" +calculator.divide(5,1));
+System.out.println("5*6 = "+calculator.multiply(5,6));
+System.out.println("6/2 = "+calculator.divide(6,2));
+System.out.println(("6/0 = "+calculator.divide(6,0)));
+
     }
 }
